206cfa560db429e4b5df2b18b5de12bad3e71a2c
abhijeetkendre500 <106870581+abhijeetkendre500@users.noreply.github.com>
2022-06-12 17:27:17 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,23 @@
+
+public class calculator {
+	
+	private int num1;
+	private int num2;
+	
+	public calculator()
+	{
+		
+	}
+	public calculator(int num1,int num2)
+	{
+		this.num1=num1;
+		this.num2=num2;
+		
+	}
+	
+	
+	
+	
+	
+
+}
